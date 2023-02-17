962eb9f169b0c8a8edee23b55289b9824834ce15
Keisuke Nishimura <mumumu0722@gmail.com>
2023-02-17 12:21:07 +0900

    Improve build interface

--- data_augmentation/AFLcem/src/run.sh
+++ data_augmentation/AFLcem/src/run.sh
@@ -2,12 +2,13 @@
 set -eux
 
 AFLCEM_TIMEOUT=${DA_MAX_TIMEOUT}
+ORIG=${TARGET_ROOT}/aflcem_target/${RELPATH}
 
 mkdir -p ${WORKDIR}/seeds
 cp ${TARGET_ROOT}/seeds/${DA_SEED} ${WORKDIR}/seeds
 
 T=`date --iso-8601=seconds`
 timeout --preserve-status --foreground ${AFLCEM_TIMEOUT} ${DA_ROOT}/afl-fuzz/afl-fuzz -C -d -m none \
-  -i ${WORKDIR}/seeds -o ${WORKDIR} -- ${WORKDIR}/${AFLCEM_DA_TARGET} ${AFLCEM_DA_ARGS}
+  -i ${WORKDIR}/seeds -o ${WORKDIR} -- ${WORKDIR}/`basename ${ORIG}`_fuzz ${ARGS}
 
 python3 ${DA_ROOT}/src/convert.py $T
