b76c8ed0a4058fdcd6d500a7c3b953d0da042068
Tomoya Kitagawa <tomoyak@ricsec.co.jp>
2023-02-17 15:27:26 +0900

    Change tmp variable prefix TMP_ to TARGET_

--- data_augmentation/AFLcem/target_build.sh
+++ data_augmentation/AFLcem/target_build.sh
@@ -10,11 +10,11 @@ fi
 
 # TODO: Run a custom build script for a specific target, if it exists.
 AFL_USE_ASAN=1 \
-TMP_LDFLAGS="-fsanitize=address" \
-TMP_CFLAGS="-fsanitize=address -static -ggdb -no-pie" \
-TMP_CXXFLAGS="-fsanitize=address -static -ggdb -no-pie" \
-TMP_CC=${DA_ROOT}/afl-fuzz/afl-gcc \
-TMP_CXX=${DA_ROOT}/afl-fuzz/afl-g++ \
+TARGET_LDFLAGS="-fsanitize=address" \
+TARGET_CFLAGS="-fsanitize=address -static -ggdb -no-pie" \
+TARGET_CXXFLAGS="-fsanitize=address -static -ggdb -no-pie" \
+TARGET_CC=${DA_ROOT}/afl-fuzz/afl-gcc \
+TARGET_CXX=${DA_ROOT}/afl-fuzz/afl-g++ \
 $1 aflcem_target
 
 ORIG=${TARGET_ROOT}/aflcem_target/${RELPATH}
