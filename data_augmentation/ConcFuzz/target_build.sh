8bcab50fa61b10b5dd528eae150cc03ee634f263
Keisuke Nishimura <mumumu0722@gmail.com>
2023-02-18 19:07:18 +0900

    Fix ConcFuzz flags

--- data_augmentation/ConcFuzz/target_build.sh
+++ data_augmentation/ConcFuzz/target_build.sh
@@ -8,11 +8,11 @@ fi
 
 # TODO: Run a custom build script for a specific target, if it exists.
 TARGET_DEF_LDFLAGS="-fsanitize=address -no-pie" \
-TARGET_DEF_CFLAGS="-fsanitize=address -ggdb -no-pie -static" \
-TARGET_DEF_CXXFLAGS="-fsanitize=address  -ggdb -no-pie -static" \
+TARGET_DEF_CFLAGS="-fsanitize=address -ggdb -no-pie" \
+TARGET_DEF_CXXFLAGS="-fsanitize=address  -ggdb -no-pie" \
 $1 oracle_source
 
 TARGET_DEF_LDFLAGS="-no-pie" \
-TARGET_DEF_CFLAGS="-ggdb -no-pie -static" \
-TARGET_DEF_CXXFLAGS="-ggdb -no-pie -static" \
+TARGET_DEF_CFLAGS="-ggdb -no-pie" \
+TARGET_DEF_CXXFLAGS="-ggdb -no-pie" \
 $1 origin_source
