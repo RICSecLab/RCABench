a87ec9809f74548cac3e493f3f23199cde903145
Tomoya Kitagawa <tomoyak@ricsec.co.jp>
2023-02-20 12:22:47 +0900

    custom build on ConcFuzz. Add a README.md
    * Run a custom build script on ConcFuzz
    * Add a README.md for adding custom build script.

--- data_augmentation/ConcFuzz/target_build.sh
+++ data_augmentation/ConcFuzz/target_build.sh
@@ -6,13 +6,26 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
-# TODO: Run a custom build script for a specific target, if it exists.
-TARGET_DEF_LDFLAGS="-fsanitize=address -no-pie" \
-TARGET_DEF_CFLAGS="-fsanitize=address -ggdb -no-pie" \
-TARGET_DEF_CXXFLAGS="-fsanitize=address  -ggdb -no-pie" \
-$1 oracle_source
+# Run a custom build script for a specific target, if it exists.
+if [ -d ${DA_ROOT}/targets/${TARGET_ID} ]; then
+  if [[ "`cat ${DA_ROOT}/targets/${TARGET_ID}/target.yaml | tr -d ' ' | grep 'run_build_sh:' | cut -d ':' -f2 `" =~ "true" ]]; then
+    TARGET_BUILD_FLAG=true
+  else
+    TARGET_BUILD_FLAG=false
+  fi
+  ${DA_ROOT}/targets/${TARGET_ID}/prebuild.sh
+else
+  TARGET_BUILD_FLAG=true
+fi
 
-TARGET_DEF_LDFLAGS="-no-pie" \
-TARGET_DEF_CFLAGS="-ggdb -no-pie" \
-TARGET_DEF_CXXFLAGS="-ggdb -no-pie" \
-$1 origin_source
+if $TARGET_BUILD_FLAG; then
+  TARGET_DEF_LDFLAGS="-fsanitize=address -no-pie" \
+  TARGET_DEF_CFLAGS="-fsanitize=address -ggdb -no-pie" \
+  TARGET_DEF_CXXFLAGS="-fsanitize=address  -ggdb -no-pie" \
+  $1 oracle_source
+
+  TARGET_DEF_LDFLAGS="-no-pie" \
+  TARGET_DEF_CFLAGS="-ggdb -no-pie" \
+  TARGET_DEF_CXXFLAGS="-ggdb -no-pie" \
+  $1 origin_source
+fi
