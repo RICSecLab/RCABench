f2d7ee5d35030943111707a49b68dbdf1e20ec43
JP3BGY <6635381+JP3BGY@users.noreply.github.com>
2023-05-11 09:31:00 +0000

    fix some issue on ConcFuzz

--- targets/bash-2018-07-msg00042/preinstall.sh
+++ targets/bash-2018-07-msg00042/preinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y --no-install-recommends build-essential bison
+apt-get install -y --no-install-recommends build-essential bison autoconf autotools-dev
